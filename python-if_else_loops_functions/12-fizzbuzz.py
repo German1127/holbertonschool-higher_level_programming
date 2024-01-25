dfc0490ed8f0f95e98b248bb1d484238d9e3351f
German1127 <kleverk2023@gmail.com>
2024-01-25 05:08:28 -0800

    task 12

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for x in range(0, 101):
+        if x % 3 == 0 and x % 5 == 0:
+            print("FizzBuzz")
+        elif x % 3 == 0:
+            print("Fizz")
+        elif x % 5 == 0:
+            print("Buzz")
+        else:
+            print(x)
