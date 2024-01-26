16b80bcf423dbb1401fc2caccbdc7ab964a30273
German1127 <kleverk2023@gmail.com>
2024-01-26 13:21:35 -0800

    task 3

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     ordenado = sorted(my_list, reverse=True)
     for ordenado in my_list:
-        print("{}".format(ordenado))
+        print("{:d}".format(ordenado))
