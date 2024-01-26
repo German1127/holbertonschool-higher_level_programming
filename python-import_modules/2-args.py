96f000d2d4971a595ace8559dc577d9646a002a8
German1127 <kleverk2023@gmail.com>
2024-01-26 06:58:23 -0800

    task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    x = len(sys.argv) - 1
+    if x == 0:
+        print("0 arguments.")
+    elif x == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(x))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
