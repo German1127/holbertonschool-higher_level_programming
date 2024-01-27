f26148a06284e858a285e0b529ad2d3b671d3af5
German1127 <kleverk2023@gmail.com>
2024-01-27 14:19:28 -0800

    task 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for x in range(len(matrix)):
+        for i in range(len(matrix[x])):
+                print("{:d}".format(matrix[x][i]), end="")
+                if i != (len(matrix[x]) - 1):
+                    print(" ", end="")
+
+        print("")
