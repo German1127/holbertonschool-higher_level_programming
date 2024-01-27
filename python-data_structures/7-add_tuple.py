3672d84f37bd4b18b33df6cd193d26443c0986f6
German1127 <kleverk2023@gmail.com>
2024-01-27 14:42:41 -0800

    task 7

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = len(tuple_a)
+    b =len(tuple_b)
+    new_tuple = ((tuple_a[0] if a >= 1 else 0) + (tuple_b[0] if b >= 1 else 0),
+                 (tuple_a[1] if a >= 2 else 0) + (tuple_b[1] if b >= 2 else 0))
+    return new_tuple
