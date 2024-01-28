8a7a350751404e610c0620067bfd876cf12a0278
German1127 <kleverk2023@gmail.com>
2024-01-28 04:10:01 -0800

    task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        l = 1
-        for j in i:
-            if l == len(i):
-                print("{:d}".format(j), end="")
-            else:
-                print("{:d}".format(j), end=" ")
-            l = l + 1
-        print()
+    for row in matrix:
+        i = 0
+        for j in row:
+            len_matrix = len(row)
+            i += 1
+            print("{:d}".format(j), end="")
+            if i != len_matrix:
+                print(" ", end="")
+        print("")
