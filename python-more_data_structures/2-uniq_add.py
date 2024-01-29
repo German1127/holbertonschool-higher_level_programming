09dc2160fcbd9af7873f91066ea83191227b78a2
German1127 <kleverk2023@gmail.com>
2024-01-29 13:29:44 -0800

    task 2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     new = my_list[:]
-    for x in range(len(my_list)):
-        new2 = sum(set(new))
-    return new2
+    sum = sum(set(new))
+    return sum
