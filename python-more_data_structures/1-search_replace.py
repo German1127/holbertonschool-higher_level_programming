46dea5f786767f667afdf6688f1c8d94e980a6be
German1127 <kleverk2023@gmail.com>
2024-01-29 12:59:57 -0800

    task 1

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for x in range(len(my_list)):
+        if new_list[x] == search:
+            new_list[x] = replace
+    return (new_list)
