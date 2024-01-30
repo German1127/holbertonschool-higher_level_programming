e76a5e81d2be59c3efca9959c338670d8401289e
German1127 <kleverk2023@gmail.com>
2024-01-30 10:18:41 -0800

    task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 def best_score(a_dictionary):
     for x in a_dictionary:
         if a_dictionary[x] >= 12:
-            return a_dictionary
+            return a_dictionary[x]
         else:
             return None
