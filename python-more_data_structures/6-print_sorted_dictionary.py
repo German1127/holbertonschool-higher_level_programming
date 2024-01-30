f5e0db35cb1f5be10e3de530a382145921be2502
German1127 <kleverk2023@gmail.com>
2024-01-30 09:09:29 -0800

    task 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    return sorted(a_dictionary.keys())
+    [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
