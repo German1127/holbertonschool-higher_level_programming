2df511b01765e0984336d2d6ebcb29c113e193c7
German1127 <kleverk2023@gmail.com>
2024-01-30 10:30:13 -0800

    task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    for x in a_dictionary:
-        if a_dictionary[x] >= 12:
-            return a_dictionary[x]
-        else:
-            return None
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
