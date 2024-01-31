ed847ac30bf54f5c1fd6bf03a08ab88bc53e07ec
German1127 <kleverk2023@gmail.com>
2024-01-31 14:34:22 -0800

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,8 +2,8 @@
 def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
-            print(my_list[i], end="")            
-        except:
+            print(my_list[i], end="")
+        except Exception:
             print()
             return i
     print()
