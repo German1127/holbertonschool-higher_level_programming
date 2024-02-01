630d0db7c1b74092d72ca4e31682a68ab13a9617
German1127 <kleverk2023@gmail.com>
2024-02-01 06:01:28 -0800

    task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""class"""
+
+
+class Square:
+    """class square"""
+
+    def __init__(self, size=0):
+        """__init__ is constructor"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.area ** 2
