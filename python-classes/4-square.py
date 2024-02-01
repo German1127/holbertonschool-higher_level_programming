56630f6a8b3e0fd1bb79d8eeeb5206b3bd7e94bd
German1127 <kleverk2023@gmail.com>
2024-02-01 06:14:00 -0800

    task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""class"""
+
+
+class Square:
+    """class square"""
+
+    def __init__(self, size=0):
+        """__init__ is constructor"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, new_size):
+        if type(new_size) is not int:
+            raise TypeError("size must be an integer")
+        if new_size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = new_size
+
+    def area(self):
+        return self.__size ** 2
