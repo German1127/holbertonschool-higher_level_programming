09fb36d13267d614f8a3aa6b2e4d0478ef73831a
German1127 <kleverk2023@gmail.com>
2024-01-31 17:47:33 -0800

    task 3

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        res = a / b
+    except ZeroDivisionError:
+        if b == 0:
+            res = None
+    finally:
+        print("{}".format(res))
+        return res
