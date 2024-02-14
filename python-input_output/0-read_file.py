01c5d1e5d9b8ee0c7d936e6f9f12d9724888e652
German1127 <kleverk2023@gmail.com>
2024-02-13 20:54:46 -0800

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""this function reads files"""
+
+
 def read_file(filename=""):
-    with open(filename, 'r', encoding="UTF-8"):
+    """the function"""
+    with open(filename, 'r', encoding="UTF-8") as p:
+        print(f"{p.read()}", end="")
