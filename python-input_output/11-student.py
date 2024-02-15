ac86dcaf07acef6127f3a43d7757a03caaadde13
German1127 <kleverk2023@gmail.com>
2024-02-15 13:11:22 -0800

    task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""class Student."""
+
+
+class Student:
+    """Student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+
+        if not isinstance(attrs, list=[]):
+            return self.__dict__
+        else:
+            dic = {}
+            for x in attrs:
+                if x in self.__dict__:
+                    dic[x] = self.__dict__[x]
+        return dic
+
+    def reload_from_json(self, json):
+        for k, v in json.items():
+            setattr(self, k, v)
