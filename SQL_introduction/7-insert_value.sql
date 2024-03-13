b3ef5baf67d2bfa69cd24a0928fcea148af57a95
German1127 <kleverk2023@gmail.com>
2024-03-13 10:17:44 -0700

    task 7

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,4 @@
--- Inserts New into the table first_table.
+-- Update 'first_table' from 'hbtn_0c_0'
+-- id = 89
+-- name = Holberton School
 INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
