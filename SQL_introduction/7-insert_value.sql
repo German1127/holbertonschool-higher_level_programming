ac83d974c71851bc6c4e3ef7918572d6e9f62abc
German1127 <kleverk2023@gmail.com>
2024-03-13 10:58:45 -0700

    task 7

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- Update 'first_table' from 'hbtn_0c_0'
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+INSERT INTO `first_table`(`id`, `name`) VALUES ('89', "Best School");
